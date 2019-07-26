7f154b3b0d52aa8fcf52a7dc0a2aa001ed2ca952
Ronald <ronald@wisekiddo.com>
2019-07-26 18:10:47 +0800

    braze setup

--- app/src/main/java/com/toolbone/braze/base/BaseApplication.kt
+++ app/src/main/java/com/toolbone/braze/base/BaseApplication.kt
@@ -1,10 +1,26 @@
 package com.toolbone.braze.base
 
 import android.app.Application
+import android.util.Log
+import com.appboy.AppboyLifecycleCallbackListener
+import com.appboy.Appboy
+import com.appboy.configuration.AppboyConfig
+import com.appboy.support.AppboyLogger
+
+
 
 class BaseApplication : Application(){
     override fun onCreate() {
         super.onCreate()
+
+        AppboyLogger.setLogLevel(Log.VERBOSE)
+
+        val appboyConfig = AppboyConfig.Builder()
+            .setDefaultNotificationChannelName("Appboy Push")
+            .setDefaultNotificationChannelDescription("Appboy related push")
+        Appboy.configure(this, appboyConfig.build())
+
+        registerActivityLifecycleCallbacks(AppboyLifecycleCallbackListener())
     }
 
 }
---
+++ app/src/main/java/com/toolbone/braze/receiver/BrazeBroadcastReceiver.kt
@@ -0,0 +1,12 @@
+package com.toolbone.braze.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BrazeBroadcastReceiver : BroadcastReceiver(){
+    override fun onReceive(p0: Context?, p1: Intent?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
